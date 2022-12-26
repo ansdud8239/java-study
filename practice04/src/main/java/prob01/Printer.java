850e4b4a916c66d64af9ef792ecb17a1f0f3dd63
ansdud8239 <ansdud8239@naver.com>
2022-12-26 09:54:36 +0900

    제너릭 메서드

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,21 +2,42 @@ package prob01;
 
 public class Printer {
 
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//		
+//	}
+//	public void println(Boolean b) {
+//		System.out.println(b);
+//		
+//	}
+//	public void println(String s) {
+//		System.out.println(s);
+//		
+//	}
+//	public void println(double d) {
+//		System.out.println(d);
+//		
+//	}
+	public <T> void println(T t) {
+		System.out.println(t);
 		
 	}
-	public void println(Boolean b) {
-		System.out.println(b);
+
+	public int sum(Integer...integers) {
+		int sum=0;
+		for(int i : integers) {
+			sum+=i;
+		}
+		return sum;
+	}
+	
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.println(t);
+		}
+	
 		
 	}
-	public void println(String s) {
-		System.out.println(s);
-		
-	}
-	public void println(double d) {
-		System.out.println(d);
-		
-	}
+	
 
 }
