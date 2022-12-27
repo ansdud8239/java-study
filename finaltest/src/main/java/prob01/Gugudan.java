d8fae11b6e6d880ced7bb2dfef13e8ab82ff597e
ansdud8239 <ansdud8239@naver.com>
2022-12-27 10:39:16 +0900

    collection

---
+++ finaltest/src/main/java/prob01/Gugudan.java
@@ -0,0 +1,15 @@
+package prob01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Gugudan {
+	private int lValue;
+	private int rValue;
+	
+	private Gugudan(int lValue,int rValue) {
+		this.lValue = lValue;
+		this.rValue = rValue;
+	}
+
+}
