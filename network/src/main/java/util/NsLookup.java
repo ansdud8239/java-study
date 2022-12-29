930349c19ab156ba50a0f7f19cfaa51246972cad
ansdud8239 <ansdud8239@naver.com>
2022-12-29 10:46:49 +0900

    과제

--- network/src/main/java/util/NsLookup.java
+++ network/src/main/java/util/NsLookup.java
@@ -2,16 +2,33 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NsLookup {
 
 	public static void main(String[] args) {
 		String line = "www.douzone.com";
+		Scanner sc = new Scanner(System.in);
 		try {
-			//주소를 스캐너로 입력받음
-			InetAddress[] inetAddresses = InetAddress.getAllByName("www.douzone.com");
-			
-			//exit로 빠져나오기
+			while (true) {
+				// 주소를 스캐너로 입력받음
+				System.out.print(">> ");
+				String dns = sc.nextLine();
+				
+				// exit로 빠져나오기
+				if (dns.equals("exit")) {
+					sc.close();
+					break;
+				}
+				
+				
+				InetAddress[] inetAddresses = InetAddress.getAllByName(dns);
+				for (InetAddress inetAddress : inetAddresses) {
+					System.out.println(dns + " : " + inetAddress.getHostAddress());
+				}
+
+			}
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
